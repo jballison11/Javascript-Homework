6750037bf2154b0555964cc1cde0e300b61b301d
John B Allison <johnballison@Johns-MBP-2.lan1>
2019-08-26 14:49:06 -0500

    second commit

--- static/js/app.js
+++ static/js/app.js
@@ -2,18 +2,33 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var t_body = d3.select("tbody");
-function createTable(tableData){
+var formControl = d3.select("#filter-btn")
+console.log(formControl)
+formControl.on("click", function(){
+  d3.event.preventDefault();
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(dataPoint => dataPoint.datetime === inputValue)
+  //console.log(filteredData)
+
+  var t_body = d3.select("tbody");
+function createTable(filteredData){
     t_body.html("")
-for (var i = 0; i < tableData.length; i ++) {
+for (var i = 0; i < filteredData.length; i ++) {
     // console.log(tableData[i])
-    var object1 = tableData[i]
+    var object1 = filteredData[i]
     var tr = t_body.append("tr")
     for (var key in object1) {
-        console.log(object1[key])
+        //console.log(object1[key])
         var td = tr.append("td")
         td.text(object1[key])
       }
 }
-}
-createTable(tableData)
+} 
+
+createTable(filteredData);
+});
+
